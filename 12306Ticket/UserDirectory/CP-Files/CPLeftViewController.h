ec4f4c23aab6317ab5832b8c14fbe7e08ab88964
CP <cp@qq.com>
2018-03-27 15:33:11 +0800

    add CPLeftViewController file

---
+++ 12306Ticket/UserDirectory/CP-Files/CPLeftViewController.h
@@ -0,0 +1,13 @@
+//
+//  CPLeftViewController.h
+//  12306Ticket
+//
+//  Created by WJG on 2018/3/27.
+//  Copyright © 2018年 WJG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPLeftViewController : UIViewController
+
+@end
