c081a72b7bc25692f202bd4d878d4764d9efd63f
WJG <12306@qq.com>
2018-03-27 15:13:30 +0800

    基础代码提交

---
+++ 12306Ticket/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  12306Ticket
+//
+//  Created by WJG on 2018/3/27.
+//  Copyright © 2018年 WJG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 12306Ticket/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  12306Ticket
+//
+//  Created by WJG on 2018/3/27.
+//  Copyright © 2018年 WJG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
