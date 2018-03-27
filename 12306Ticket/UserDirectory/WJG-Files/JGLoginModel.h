e30b4de95dfe3956b5d2a5aca9d6353de56b52f9
CP <cp@qq.com>
2018-03-27 15:26:49 +0800

    add JGLoginModel file

---
+++ 12306Ticket/UserDirectory/WJG-Files/JGLoginModel.h
@@ -0,0 +1,13 @@
+//
+//  JGLoginModel.h
+//  12306Ticket
+//
+//  Created by WJG on 2018/3/27.
+//  Copyright © 2018年 WJG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JGLoginModel : NSObject
+
+@end
